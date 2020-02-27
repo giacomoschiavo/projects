f10e316feff5b761b465435b382d78993b377101
giaco <42863424+SeriousStuff@users.noreply.github.com>
2020-02-27 13:30:59 +0100

    Summary
    Added logic

--- KNN-classifier/sketch.js
+++ KNN-classifier/sketch.js
@@ -1,17 +1,24 @@
 let video;
 let features;
 let knn;
-let labelP;
+let resultP;
+let canvas;
+let input;
 let ready = false;
 
 function setup() {
-  createCanvas(320, 240);
+  input = {
+    w: select("#input").width,
+    h: select("#input").width / 4 * 3
+  };
+  canvas = createCanvas(input.w, input.h);
+  canvas.parent("input");
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(input.w, input.h);
   video.hide();
   features = ml5.featureExtractor("MobileNet", video, modelReady);
   knn = ml5.KNNClassifier();
-  labelP = createP("need data");
+  resultP = select("#result").html("need data");
 }
 
 function modelReady() {
@@ -24,31 +31,38 @@ function goClassify() {
     if (error) {
       console.error(error);
     } else {
-      labelP.html(result.label);
+      resultP.html(result.label);
+      select("#test1").style("width", result.confidences[0].toFixed(3)*100 + "%");
+      select("#test2").style("width", result.confidences[1].toFixed(3)*100 + "%");
+      select("#test3").style("width", result.confidences[2].toFixed(3)*100 + "%");
+      // console.log(result);
       goClassify();
     }
   })
 }
 
-
-
-function keyPressed() {
-  const logits = features.infer(video);
-  if (key == "a") {
-    knn.addExample(logits, "left");
-  } else if (key == "d") {
-    knn.addExample(logits, "right");
-  } else if (key == "s") {
-    knn.addExample(logits, "center");
-  }
-}
-
 function draw() {
   image(video, 0, 0, width, height);
 
-  if(!ready && knn.getNumLabels() > 0) {
+  if(knn.getNumLabels() == 3 && !ready) {
     goClassify();
     ready = true;
   }
-  
+}
+
+function windowResized() {
+  video.size(input.w, input.h);
+  resizeCanvas(input.w, input.h);
+}
+
+function test1add() {
+  knn.addExample(features.infer(video), "test1");
+}
+
+function test2add() {
+  knn.addExample(features.infer(video), "test2");
+}
+
+function test3add() {
+  knn.addExample(features.infer(video), "test3");
 }
