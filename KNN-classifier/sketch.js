500aba6273bd13818417d3614842fb21bad3ccad
giaco <42863424+SeriousStuff@users.noreply.github.com>
2020-02-27 15:59:23 +0100

    Summary
    migliorata visualizzazione webcam (ora quadrata)
    aggiunta visualizzazione foto per classe
    aggiunta personalizzazione del colore per classe

--- KNN-classifier/sketch.js
+++ KNN-classifier/sketch.js
@@ -6,24 +6,30 @@ let canvas;
 let input;
 let ready = false;
 let classes = [];
+let e;
 
 function setup() {
   input = {
     w: select("#input").width,
-    h: select("#input").width / 4 * 3
+    h: select("#input").width
   };
   canvas = createCanvas(input.w, input.h);
   canvas.parent("input");
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO, cameraLoaded);
   video.size(input.w, input.h);
   video.hide();
   features = ml5.featureExtractor("MobileNet", video, modelReady);
   knn = ml5.KNNClassifier();
   resultP = select("#result").html("need data");
+  addClass("ciao", "blue");
 }
 
 function modelReady() {
   console.log("Model is ready");
+}
+
+function cameraLoaded() {
+  console.log("Camera is ready");
 }
 
 function goClassify() {
@@ -33,17 +39,21 @@ function goClassify() {
       console.error(error);
     } else {
       resultP.html(result.label);
-      // select("#test1").style("width", result.confidences[0].toFixed(3) * 100 + "%");
-      // select("#test2").style("width", result.confidences[1].toFixed(3) * 100 + "%");
-      // select("#test3").style("width", result.confidences[2].toFixed(3) * 100 + "%");
-      // console.log(result);
+      updateBar(result);
       goClassify();
     }
   })
 }
 
+function updateBar(result) {
+  let results = result.confidencesByLabel;
+  for (let item in results) {
+    select("#" + item).style("width", results[item].toFixed(3) * 100 + "%")
+  }
+}
+
 function draw() {
-  image(video, 0, 0, width, height);
+  image(video, -80, -0, width + 80, height + 0);
 
   if (knn.getNumLabels() >= 2 && !ready) {
     goClassify();
@@ -56,24 +66,35 @@ function windowResized() {
   resizeCanvas(input.w, input.h);
 }
 
-function addClass(name, color="yellow") {
-  if (exist(name) || classes.length > 4) return;
+function addClass(name, color = "yellow") {
+  if (exist(name) || classes.length > 4 || name == "") return;
 
   let classDiv = createDiv();
   classDiv.class("class");
 
   let loadDiv = createDiv();
   loadDiv.id(name);
+  loadDiv.class("confidence");
   loadDiv.style("background-color", color);
 
   let trainButton = createButton("Train \"" + name + "\"");
   trainButton.class("trainButton");
-  trainButton.elt.addEventListener("mousedown", () => {
-    console.log(1)
+  trainButton.elt.addEventListener("mousedown", (ev) => {
+    // e = ev;
+    let children = ev.path[1].children;
+    for(let i = 0; i < children.length; i++) {
+      if(children[i].className == "snapshots") {
+        children[i].prepend(capture());
+      }
+    }
     knn.addExample(features.infer(video), name);
   });
 
+  let imgDiv = createDiv();
+  imgDiv.class("snapshots");
+
   classDiv.child(loadDiv);
+  classDiv.child(imgDiv);
   classDiv.child(trainButton);
 
   classDiv.parent(select("#training"));
@@ -91,5 +112,13 @@ function exist(name) {
 }
 
 function onAddClass() {
-  addClass(select("#inputClassName").value());
+  addClass(select("#inputClassName").value(), select("#colorClass").value());
+  select("#inputClassName").value("");
+  select("#colorClass").value("#666666");
+}
+
+function capture() {
+  let img = new Image();
+  img.src = canvas.elt.toDataURL();
+  return img;
 }
