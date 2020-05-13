d6deb19ab0c0db4814b309958ccafea6cf2d939b
Giacomo Schiavo <giacomo.schiavo12@gmail.com>
2020-05-13 20:14:47 +0200

    create ressebd-client
    initial commit

---
+++ ressend-client/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Resssend🐍',
+    },
+  },
+  devServer: {
+    host: 'localhost'
+  },
+};
